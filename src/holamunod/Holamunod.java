1a284dd09efaf8c8d6af9c91bf15fa99fe318868
Dylan <Cardenasdylan58@gmail.com>
2025-04-09 18:57:17 -0500

    YEPA

---
+++ src/holamunod/Holamunod.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamunod;
+
+
+public class Holamunod {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola munod");    
+        System.out.println("A");
+    }
+    
+}
